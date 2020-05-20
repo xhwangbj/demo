47610572dc8c54d3120c05367ca83452c8d7a280
xiaohang_w <xhwangbj@cn.ibm.com>
2020-05-20 05:17:02 +0000

    116

--- TESTA.cbl
+++ TESTA.cbl
@@ -3,10 +3,10 @@
        DATA DIVISION.                                  
        WORKING-STORAGE SECTION.                        
        01 WS-COMMAREA.                                 
-           03 DATAFLD2                  PIC X(20).  
+           03 DATAFLD3                  PIC X(20).  
        LINKAGE SECTION.                                
        01 DFHCOMMAREA PIC X(20).                       
        PROCEDURE DIVISION.                             
-           MOVE 'NORTH AMERICAN' TO DATAFLD2.           
+           MOVE 'NORTH AMERICAN' TO DATAFLD3.           
            MOVE WS-COMMAREA TO DFHCOMMAREA.            
        EXEC CICS RETURN END-EXEC.                      
