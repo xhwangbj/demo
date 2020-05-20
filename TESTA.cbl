be4c7ebdd0ed02f87e70ac16fe50ccb7337863cd
xiaohang_w <xhwangbj@cn.ibm.com>
2020-05-20 05:16:03 +0000

    33

--- TESTA.cbl
+++ TESTA.cbl
@@ -3,7 +3,7 @@
        DATA DIVISION.                                  
        WORKING-STORAGE SECTION.                        
        01 WS-COMMAREA.                                 
-           03 DATAFLD2                  PIC X(20).                                
+           03 DATAFLD2                  PIC X(20).  
        LINKAGE SECTION.                                
        01 DFHCOMMAREA PIC X(20).                       
        PROCEDURE DIVISION.                             
